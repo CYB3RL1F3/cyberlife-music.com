226c5cdf601f87d301b27af8d204490fe59d8c1f
CYB3RL1F3 <cyberlife.music@gmail.com>
2022-12-11 03:50:49 +0100

    remove pointless file

--- app/routes/not-supported.tsx
+++
@@ -1,7 +0,0 @@
-import NotSupportedPage from "~/components/pages/NotSupportedPage";
-import { isIe } from "~/utils/browser";
-
-export default function NotSupportedRoute() {
-  const isTheWorstBrowserOfThePlanet = isIe();
-  return <NotSupportedPage isIe={isTheWorstBrowserOfThePlanet} />;
-}
