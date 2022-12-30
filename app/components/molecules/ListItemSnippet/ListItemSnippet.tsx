b9d2ee15a2ee8b5b965c340771d817815d74375b
CYB3RL1F3 <cyberlife.music@gmail.com>
2022-12-30 19:23:52 +0100

    fix ellipsis

--- app/components/molecules/ListItemSnippet/ListItemSnippet.tsx
+++ app/components/molecules/ListItemSnippet/ListItemSnippet.tsx
@@ -3,7 +3,7 @@ import type { ListItemSnippetProps } from "./ListItemSnippet.types";
 
 const ListItemSnippet = ({ children, title, href }: ListItemSnippetProps) => {
   const cls =
-    "italic font-semibold text-right uppercase text-sm md:text-md cursor:pointer hover:text-white";
+    "italic font-semibold text-right uppercase text-sm md:text-md cursor:pointer hover:text-white line-clamp-1 pr-2";
   return (
     <div className="flex flex-col w-full gap-2 py-2 flex-end">
       {href ? (
