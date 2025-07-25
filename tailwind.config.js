d06a2ee9d2fd412a88e0394804c6ff10b2b0fa9b
CYB3RL1F3 <cyberlife.music@gmail.com>
2025-07-25 02:52:48 +0200

    fix bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,83 +1,83 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  mode: 'jit',
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screen: {
-      mobile: "740px",
-      tablet: "1024px",
-      desktop: "1420px",
-      "max-md": { max: "767px" }
+      mobile: '740px',
+      tablet: '1024px',
+      desktop: '1420px',
+      'max-md': { max: '767px' },
     },
     extend: {
       screen: {
-        sm: "740px",
-        md: "1024px",
-        lg: "1420px",
-        "max-md": { max: "767px" }
+        sm: '740px',
+        md: '1024px',
+        lg: '1420px',
+        'max-md': { max: '767px' },
       },
       colors: {
         gray: {
-          100: "#DEDEDE",
-          200: "#CCCCCC",
-          300: "#AAAAAA",
-          400: "#999999",
-          500: "#666666",
-          600: "#444444",
-          700: "#333333",
-          800: "#222222",
-          900: "#111111"
+          100: '#DEDEDE',
+          200: '#CCCCCC',
+          300: '#AAAAAA',
+          400: '#999999',
+          500: '#666666',
+          600: '#444444',
+          700: '#333333',
+          800: '#222222',
+          900: '#111111',
         },
-        lightGray: "#999999",
-        lightPink: "#F9C3C5"
+        lightGray: '#999999',
+        lightPink: '#F9C3C5',
       },
       width: {
-        inherit: "inherit"
+        inherit: 'inherit',
       },
       height: {
-        screen: "calc(var(--vh) * 100)"
+        screen: 'calc(var(--vh) * 100)',
       },
       keyframes: {
         appear: {
-          "0%": { transform: "translateX(50vw)" },
-          "80%": { transform: "translateX(-100px)" },
-          "100%": { transform: "translateX(0)" }
+          '0%': { transform: 'translateX(50vw)' },
+          '80%': { transform: 'translateX(-100px)' },
+          '100%': { transform: 'translateX(0)' },
         },
         opacity: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 }
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
         },
         loading: {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(360deg)" }
-        }
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
       },
       animation: {
-        appear: "appear 300ms ease-in-out forwards",
-        opacity: "opacity 300ms ease-in-out forwards",
-        loading: "loading 150ms linear infinite"
+        appear: 'appear 300ms ease-in-out forwards',
+        opacity: 'opacity 300ms ease-in-out forwards',
+        loading: 'loading 150ms linear infinite',
       },
       backgroundImage: {
-        main: 'url("https://res.cloudinary.com/hw2jydiif/image/upload/v1667476701/btby2qfnqpbpnnfpzdt5.webp")'
+        main: 'url("https://cdn.cyberlife-music.com/images/cyberlife-bg.jpg")',
       },
       fontSize: {
-        lg: "16pt",
-        md: "12pt",
-        sm: "10pt",
-        xl: "20pt",
-        xxl: "70pt",
-        semilarge: "35pt",
-        large: "40pt"
+        lg: '16pt',
+        md: '12pt',
+        sm: '10pt',
+        xl: '20pt',
+        xxl: '70pt',
+        semilarge: '35pt',
+        large: '40pt',
       },
       fontFamily: {
-        "myriad-pro": "myriad-pro"
-      }
-    }
+        'myriad-pro': 'myriad-pro',
+      },
+    },
   },
   plugins: [
-    require("tailwindcss-owl"),
-    require("tailwindcss-animation-delay"),
-    require("@tailwindcss/line-clamp"),
-    require("tailwind-scrollbar")({ nocompatible: true })
-  ]
+    require('tailwindcss-owl'),
+    require('tailwindcss-animation-delay'),
+    require('@tailwindcss/line-clamp'),
+    require('tailwind-scrollbar')({ nocompatible: true }),
+  ],
 };
