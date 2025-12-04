26e52255a14e497cf0f3518e9b3126a160f57964
CYB3RL1F3 <cyberlife.music@gmail.com>
2025-12-05 00:05:53 +0100

    remove useless deps

--- app/hooks/styles/useLinkStyle.ts
+++ app/hooks/styles/useLinkStyle.ts
@@ -2,13 +2,10 @@ import { cn } from '~/utils/cn';
 import { useMemo } from 'react';
 
 export const useLinkStyle = (className?: string) => {
-  const buttonClassName = useMemo(
+  const linkClassName = useMemo(
     () =>
-      cn(
-        'font-semibold leading-6 cursor-pointer hover:text-white',
-        className,
-      ),
+      cn('font-semibold leading-6 cursor-pointer hover:text-white', className),
     [className],
   );
-  return buttonClassName;
+  return linkClassName;
 };
